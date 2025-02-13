f359869964556bfc4ed31ce973a54648cd8686a3
Youssef Bounite <ybounite@c3r8p3.1337.ma>
2025-02-13 20:52:46 +0100

    add in heder file

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 15:29:53 by ybounite          #+#    #+#             */
-/*   Updated: 2025/02/11 16:45:17 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/02/12 14:48:38 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@
 # include <signal.h>
 # include <unistd.h>
 # include <sys/types.h>
+
+# define YELLOW	"\e[1;33m"
+# define RED     "\e[1;31m"
+# define GREEN   "\e[1;32m"
+# define BLUE    "\e[1;34m"
+# define WHITE   "\e[1;37m"
 
 void	send_char(int pid, unsigned char c);
 void	action(int sig);
